6d32a62fd2b0ad5a8ea74dc2f5ce3b0eb3fc4261
“BartkiewiczM” <228684@edu.p.lodz.pl>
2023-04-07 12:13:50 +0200

    count_elements and validate_passowrd classes implemented

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  def initialize(arr)
+    @arr = arr
+  end
 
+  def perform
+    result = Hash.new(0)
+    @arr.each { |elem| result[elem] += 1 }
+    result
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,15 @@
 class ValidatePassword
+  def initialize(password)
+    @password = password
+  end
 
+  def perform
+    return false if @password.length < 6 || @password.length > 24
+    return false unless @password.match(/[A-Z]/)
+    return false unless @password.match(/[a-z]/)
+    return false unless @password.match(/[0-9]/)
+    return false unless @password.match(/[!@#\$%\^&\*\(\)_\+\-=\[\]{};':"\\|,.<>\/\?]/)
+    return false if @password.scan(/(\w)\1{2,}/).any?
+    true
+  end
 end
